ec3d1c8e307404d3477a6bf52d560db28aa98c4c
pduren <57735971+kParkz@users.noreply.github.com>
2022-05-12 10:14:09 -0500

    Separated scale 1 vs 4 logic. Scale 1 logic: crank and poll for zoom button. Scale 4: d-pad movement and exit scale 4 back to scale 1

--- source/main.lua
+++ source/main.lua
@@ -28,19 +28,68 @@ roomOneSetUp()
 
 function playdate.update()
 
-	-- get crank change
-	local change = playdate.getCrankChange(change, acceleratedChange);
+	--check to see if we're in zoomed out state
+	if (playdate.display.getScale() == 1) then
 
-	local x,y = gfx.sprite.getPosition(roomSprite);
+		-- get crank change
+		local change = playdate.getCrankChange(change, acceleratedChange);
 
-	-- move the roomSprite by the crank change value
-	-- also check to make sure the image position doesn't go out of bounds, bounce back a pixel if so
-	if (y < 359 and y > -120) then
- 		roomSprite:moveBy( 0, change )
-	elseif (y > 359) then
-		roomSprite:moveBy( 0, -1 )
-	elseif (y < -120) then
-		roomSprite:moveBy( 0, 1 )
+		local x,y = gfx.sprite.getPosition(roomSprite);
+
+		-- move the roomSprite by the crank change value
+		-- also check to make sure the image position doesn't go out of bounds, bounce back a pixel if so
+		if (y < 359 and y > -120) then
+			roomSprite:moveBy( 0, change )
+		elseif (y > 359) then
+			roomSprite:moveBy( 0, -1 )
+		elseif (y < -120) then
+			roomSprite:moveBy( 0, 1 )
+		end
+
+		-- set the scale to zoom
+
+		-- enter zoomed state
+		if playdate.buttonIsPressed(playdate.kButtonUp) then
+
+			playdate.display.setScale(4)
+
+			-- wait for a sec so that the up input doesn't propogate down to zoomed state
+			playdate.wait(100)
+
+		end
+
+	end
+	
+
+
+	-- check if we're zoomed. If so, do zoom unique controls
+	if (playdate.display.getScale() == 4) then
+		-- d-pad movement
+
+		if playdate.buttonIsPressed( playdate.kButtonUp ) then
+			roomSprite:moveBy( 0, 2 )
+		end
+		if playdate.buttonIsPressed( playdate.kButtonRight ) then
+			roomSprite:moveBy( -2, 0 )
+		end
+		if playdate.buttonIsPressed( playdate.kButtonDown ) then
+			roomSprite:moveBy( 0, -2 )
+		end
+		if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+			roomSprite:moveBy( 2, 0 )
+		end
+
+		-- if press a then check area for enemy
+		-- if enemy found then set scale to 1, begin fight sequence and exit zoom
+
+		-- exit zoomed state if B button is pressed
+		if playdate.buttonIsPressed(playdate.kButtonB) then
+
+			playdate.display.setScale(1)
+			roomSprite:moveTo( 200, 120 )
+
+		end
+
 	end
 
     gfx.sprite.update()
